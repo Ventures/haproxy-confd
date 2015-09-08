2f80571c9b3f3a6a58a7d1433fd002e2c42fa6d0
Jussi Nummelin <jussi.nummelin@digia.com>
2015-09-08 09:33:37 +0300

    WIP: Added cert handling

--- cert_splitter.sh
+++ cert_splitter.sh
@@ -4,4 +4,5 @@ cd /tmp
 csplit -s -f cert -b %02d_gen.pem certs.pem "/$CERT_SPLIT_TOKEN/+1"
 rm /etc/haproxy/certs/cert*_gen.pem
 mv cert*_gen.pem /etc/haproxy/certs/
+etcdctl set --peers $ETCD_NODE /haproxy-$HAPROXY_ID/certs true
 echo "...done. New certificates updated into HAProxy."
